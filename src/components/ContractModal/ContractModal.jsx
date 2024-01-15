79848601d980e65c44f0d835b8294a0c2005e0da
Brock Nelson <Bmnelson87@gmail.com>
2024-01-15 08:44:36 -0600

    modal setup for adding/editing contract, need to add form next

--- src/components/ContractModal/ContractModal.jsx
+++ src/components/ContractModal/ContractModal.jsx
@@ -6,9 +6,31 @@ function ContractModal ({ closeModal, defaultValue }) {
 
     const dispatch = useDispatch();
 
+    let [contract, setContract] = useState(defaultValue || {contract_id: "", contracted_amt: 0, price: 0, location_name: "", crop: "", delevery_month: "", status: ""});
+
+    const handleChangeFor = (key, value) => {
+        setContract({ ...contract, [key]: value })
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (defaultValue === null) {
+            dispatch({ type: 'SEND_CONTRACT_TO_SERVER', payload: contract });
+        } else {
+            dispatch({ type: 'UPDATE_CONTRACT', payload: contract });
+        }
+        setContract({contract_id: "", contracted_amt: 0, price: 0, location_name: "", crop: "", delevery_month: "", status: ""});
+        closeModal();
+    }
+
     return(
-        <div>
-            <p>Contract Modal</p>
+        <div className="modal-container" onClick={(e) => {
+            if(e.target.className === "modal-container") closeModal()
+        }}>
+            <div className="modal"> 
+                <p>Contract Modal</p>
+            </div>
+           
         </div>
     );
 };
