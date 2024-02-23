7a6d055ee9ff6c8778a6ba1944a31de71a7ed666
Brock Nelson <Bmnelson87@gmail.com>
2024-02-23 17:43:13 -0600

    fixed database working on readme

--- database.sql
+++ database.sql
@@ -16,14 +16,6 @@ CREATE TABLE "crops" (
 CREATE TABLE "contracts" (
 	"id" SERIAL PRIMARY KEY,
 	"user_id" INT REFERENCES "user" NOT NULL,
-	"price" INT,
-	"crop" VARCHAR (12),
-	"yield" INT
-);
-
-CREATE TABLE "contracts" (
-	"id" SERIAL PRIMARY KEY,
-	"user_id" INT REFERENCES "user" NOT NULL,
 	"crop_id" INT REFERENCES "crops",
 	"contract_id" VARCHAR(255),
 	"amount" INT,
@@ -32,23 +24,3 @@ CREATE TABLE "contracts" (
 	"month" varchar(12),
 	"status" VARCHAR (12) DEFAULT 'Open'
 );
-
-INSERT INTO "contracts" ("user_id", "contract_id", "amount", "price", "location", "month")
-	VALUES 
-(1, 'ABS1337', 10000, 5.00, 'Absolute Energy Lyle','December'),
-(1, 'VAR420', 9000, 5.50, 'Charles City Valero','March'),
-(1, 'CHS8008', 2100, 13.00, 'Fairmont CHS','August')
-;
-
-INSERT INTO "crops" ("user_id", "year", "crop", "yield", "source")
-	VALUES
-	(1, 2020, 'Corn', 15231, 'Actual'),
-	(1, 2020, 'Soybeans', 5539, 'Actual'),
-	(1, 2021, 'Corn', 7594, 'Actual'),
-	(1, 2021, 'Soybeans', 10923, 'Actual'),
-	(1, 2022, 'Corn', 17927, 'Actual'),
-	(1, 2022, 'Soybeans', 5709, 'Actual'),
-	(1, 2023, 'Corn', 6291, 'Monitor'),
-	(1, 2023, 'Soybeans', 8842, 'Monitor'),
-	(1, 2024, 'Corn', 16500, 'Projected')
-;
